d6da365ec24523e479a723b94fa17f49b25c6f04
Tamaraius <tamara.galovic@harness.io>
2022-03-17 14:49:50 +0100

    fix: move string to module level

--- src/stringTypes.ts
+++ src/stringTypes.ts
@@ -3406,6 +3406,7 @@ export interface StringsMap {
   'cd.setupGitOpsServerStep': string
   'cd.specifyTfVarStore': string
   'cd.stepName': string
+  'cd.steps.azureInfraStep.azure': string
   'cd.steps.azureInfraStep.resourceGroupPlaceholder': string
   'cd.steps.azureInfraStep.subscriptionPlaceholder': string
   'cd.steps.common.clusterDetails': string
@@ -7713,10 +7714,6 @@ export interface StringsMap {
    * @deprecated migrate this string to module level file
    */
   'pipelineSteps.deploy.serviceSpecifications.useFromStageLabel': string
-  /**
-   * @deprecated migrate this string to module level file
-   */
-  'pipelineSteps.deploymentTypes.azure': string
   /**
    * @deprecated migrate this string to module level file
    */
